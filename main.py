a3b36f6d2f14a75dff43f94afb94db24ce39ef57
Tahmauri <tmbo718@gmail.com>
2026-02-23 01:36:15 -0500

    Added book adding and looking up books

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ def main():
     #initializes book list
     bookList = []
 
+    choice = 0
 
+    #keeps running until user quits
+    while choice != 4:
+        print("*** Book Manager *** \n1) Add a book\n2) Lookup a book\n3) Display all books\n4) Quit")
+        choice = int(input())
 
-
+        #adds book
+        if choice == 1:
+            print("Adding book...")
+            nBook = input("Enter Book Name: ")
+            nAuthor = input("Enter Author Name:")
+            nPages = input("Enter Number of Pages:")
+            bookList.append([nBook, nAuthor, nPages])
+        
+        #looks for book
+        elif choice == 2:
+            print("Looking for a book...")
+            keyword = input("Enter Search Term: ")
+            for book in bookList:
+                if keyword in book:
+                    print(book)
+                else:
+                    print("Book cannot be found.")
 
 
 if __name__ == "__main__":
