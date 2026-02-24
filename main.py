7bfa2e277ace0ba602369c1da84a0f3ac4366cca
Tahmauri <tmbo718@gmail.com>
2026-02-24 04:35:37 -0500

    allows book list to save and be recalled to txt file after quitting program, completed project

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
 
-    #initializes book list
-    bookList = []
+    #initializes book list, calls to the the txt file, creates new file if one doesnt exist
+
+    try:
+        bookList = []
+        infile = open("theBookList.txt", "r")
+        line = infile.readline()
+        while line:
+            bookList.append(line.rstrip("\n").split(","))
+            line = infile.readline()
+        infile.close()
+    except FileNotFoundError:
+        print("theBookList.txt file does not exist,\nCreating a new book list")
+        bookList = []
 
     choice = 0
 
@@ -31,8 +42,8 @@ def main():
             for book in bookList:
                 if keyword in book:
                     print(f"{" | ".join(book[:2])} | {book[2]} pages")
-                else:
-                    print("Book cannot be found.")
+                    break
+
 
         #displays all book
         elif choice == 3:
@@ -49,6 +60,14 @@ def main():
         else:
             print("\nEnter a valid choice.")
     print("Ended")
+
+    #Saving to external txt file
+
+    outfile = open("theBookList.txt", "w")
+    for book in bookList:
+        outfile.write(",".join(book) + "\n")
+    outfile.close()
+
 
 if __name__ == "__main__":
     main()
